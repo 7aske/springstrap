2df41f2c3624e80d583d0edc9678af3c85442be2
Nikola Tasic <ntasic7@gmail.com>
2020-08-31 02:25:21 +0200

    refactor

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+const {NODE_ENV = "production"} = process.env;
+
+module.exports = {
+	entry: "./build/index.js",
+	mode: NODE_ENV,
+	target: "node",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+	},
+	resolve: {
+		extensions: [".js"],
+	},
+	optimization: {
+		minimize: NODE_ENV === "production",
+		minimizer: [new TerserPlugin({parallel: 4})],
+	},
+};
